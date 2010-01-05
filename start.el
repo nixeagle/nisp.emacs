8169f2bcccc424185bfdb3a46a1a9a9e25b238c0
james <james@lappy.(none)>
2010-01-05 01:49:34 +0000

    include magit and ido mode

--- start.el
+++ start.el
@@ -5,12 +5,21 @@
 (load custom-file)
 
 ;;; Setup some load paths
-(add-to-list 'load-path "~/.emacs.d/nixp.emacs")
-(add-to-list 'load-path "~/.emacs.d/nixp.emacs/my")
+(add-to-list 'load-path "~/.emacs.d/nisp.emacs/")
+(add-to-list 'load-path "~/.emacs.d/nisp.emacs/my")
 (add-to-list 'load-path "~/.emacs.d/nisp.emacs/3rd-party/jwiegley-magit")
 
 
 ;;; Now begin loading in custom lisp.
 (load "~/.emacs.d/nisp.emacs/nisp-old.el")
 
+;;;; Requires
+;;; Lets try loading the keymap first
+(require 'my-keymap)
+
+;;; Include and turn on ido-mode
+(require 'ido)
+(ido-mode 1)
+
+;;; Include magit. This is a special submodule
 (require 'magit)
