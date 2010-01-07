d7da30723fa138759f55e368450a96d812d4f696
James S <i@nixeagle.org>
2010-01-07 23:46:31 +0000

    rename some variables and copy over some erc things

--- nisp-old.el
+++ nisp-old.el
@@ -7,7 +7,7 @@
          ((string= file ".") t)
          ((string= file "..") t)
          ((file-directory-p path) ; keep going
-          (setq results 
+          (setq results
                 (append (my-recursive-file-regexp path regexp)
                         results)))
          ((string-match regexp path)
@@ -23,11 +23,11 @@
 
 (defun my-list-directories-with-file (root-dir regexp)
   "List all directories a file matching REGEXP was found.
-  
+
   Start recursion with `my-recursive-file-regexp' at ROOT-DIR"
   (my-unique-list
-     (mapcar 'file-name-directory
-             (my-recursive-file-regexp root-dir regexp))))
+   (mapcar 'file-name-directory
+           (my-recursive-file-regexp root-dir regexp))))
 
 (defun my-add-subdirs-to-load-list (parent-dir regexp)
   "Given PARENT-DIR add subdirs with .el files to `load-list'."
@@ -41,31 +41,31 @@
 ;; these are imported into erc and can be run with /OPME etc.
 ;; TODO: see if I can put my standard prefix in (nix:).
 (defun erc-cmd-OPME ()
-        "Request a chanop op to me in current channel."
-        (erc-message "PRIVMSG" 
-            (format "chanserv op %s %s"
-                    (erc-default-target)
-                    (erc-current-nick)) nil))
+  "Request a chanop op to me in current channel."
+  (erc-message "PRIVMSG"
+               (format "chanserv op %s %s"
+                       (erc-default-target)
+                       (erc-current-nick)) nil))
 
-(defun erc-cmd-DEOPME ()    
-        "Deop myself from current channel."
-        (erc-cmd-DEOP (format "%s" (erc-current-nick))))
+(defun erc-cmd-DEOPME ()
+  "Deop myself from current channel."
+  (erc-cmd-DEOP (format "%s" (erc-current-nick))))
 
 
 (defun erc-cmd-EEVAL (&rest expression)
   "evaluate given lisp expression."
   (let* ((expr (mapconcat 'identity expression " ")) ;(mapconcat 'identity expression " "))
-	 (result (condition-case err
-		     (eval (read-from-whole-string expr))
-		   (error (format "ERROR: %S" error)))))
+         (result (condition-case err
+                     (eval (read-from-whole-string expr))
+                   (error (format "ERROR: %S" error)))))
     (erc-send-message (format "%s ---> %S" expr result))))
 (defalias 'erc-cmd-E 'erc-cmd-EEVAL)
 (defun erc-cmd-SEVAL (&rest expression)
   "evaluate given lisp expression."
   (let* ((expr (mapconcat 'identity expression " ")) ;(mapconcat 'identity expression " "))
-	 (result (condition-case err
-		     (sawfish-eval-noread (read-from-whole-string expr))
-		   (error (format "ERROR: %S" error)))))
+         (result (condition-case err
+                     (sawfish-eval-noread (read-from-whole-string expr))
+                   (error (format "ERROR: %S" error)))))
     (erc-send-message (format "%s ---> %s" expr result))))
 
 
@@ -76,17 +76,17 @@
 See `nix-erc-get-buffer' for details on how SERVER and TARGET are handled."
   (declare (indent 1) (debug t))
   (let ((buf (make-symbol "buf")))
-	`(let* ((,buf (nisp-erc-get-buffer ,target ,server)))
-		(if (eq ,buf nil) (nil)
-		  (with-current-buffer ,buf 
-		    ,@body)))))
+    `(let* ((,buf (nisp-erc-get-buffer ,target ,server)))
+       (if (eq ,buf nil) (nil)
+         (with-current-buffer ,buf
+           ,@body)))))
 
 (defun nisp-erc-send-message (channel message &optional server)
   "Send a MESSAGE to CHANNEL.
 If SERVER is given send message to CHANNEL on that SERVER,
 otherwise send message to the first CHANNEL on any server"
   (nisp-erc-with-current-buffer server channel
-			       (erc-send-message message)))
+                                (erc-send-message message)))
 
 
 (defun nisp-erc-get-buffer (target &optional server-name)
@@ -107,13 +107,43 @@ IF TARGET is nil and SERVER-NAME is passed, return related server buffer"
 (defun nisp-erc-get-server-proc (&optional server-name)
   "Return server process matching SERVER-NAME or return nil on failure"
   (catch 'info
-    (mapcar (lambda (proc) 
-	      (let ((host (process-contact proc :host)))
-		(cond 
-		 ((string-equal server-name host)
-		  (throw 'info proc)))))
-	    (process-list))
+    (mapcar (lambda (proc)
+              (let ((host (process-contact proc :host)))
+                (cond
+                 ((string-equal server-name host)
+                  (throw 'info proc)))))
+            (process-list))
     nil))				;no match return nil.
+
+(require 'my-erc-pass)
+(defun my-erc-connect-eighthbit ()
+  "Connect me to eighthbit as nixeaglemacs"
+  (interactive)
+  (erc :server "platinum.eighthbit.net"
+       :nick "nixeagle"
+       :full-name "James"
+       :port "6667"
+       :password *my-erc-eighthbit-pass*))
+
+(defun my-erc-connect-freenode ()
+  "Connect to freenode using my nick"
+  (interactive)
+  (erc :server "irc.freenode.org"
+       :nick "nixeagle"
+       :full-name "James"
+       :port "6667"
+       :password *my-erc-freenode-pass*))
+
+(defun my-erc-connect-oftc ()
+  "Connect to freenode using my nick"
+  (interactive)
+  (erc :server "irc.oftc.net"
+       :nick "nixeagle"
+       :full-name "James"
+       :port "6667"
+       :password *my-erc-oftc-pass*))
+
+(defalias 'my-erc-connect-debian 'my-erc-connect-oftc)
 
 
 (provide 'nisp-old)
