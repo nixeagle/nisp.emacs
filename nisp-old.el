9dae1309569c398a63e93955e08389d7f6aac08b
james <james@localhost.(none)>
2010-01-07 00:56:50 +0000

    importing a bunch of old erc related commands and the scp paste stuff (not working until emacs goodies is installed).

--- nisp-old.el
+++ nisp-old.el
@@ -35,4 +35,85 @@
             (add-to-list 'load-path arg))
           (my-list-directories-with-file parent-dir regexp)))
 
+
+;;;; old erc stuff
+;;; Defined commands
+;; these are imported into erc and can be run with /OPME etc.
+;; TODO: see if I can put my standard prefix in (nix:).
+(defun erc-cmd-OPME ()
+        "Request a chanop op to me in current channel."
+        (erc-message "PRIVMSG" 
+            (format "chanserv op %s %s"
+                    (erc-default-target)
+                    (erc-current-nick)) nil))
+
+(defun erc-cmd-DEOPME ()    
+        "Deop myself from current channel."
+        (erc-cmd-DEOP (format "%s" (erc-current-nick))))
+
+
+(defun erc-cmd-EEVAL (&rest expression)
+  "evaluate given lisp expression."
+  (let* ((expr (mapconcat 'identity expression " ")) ;(mapconcat 'identity expression " "))
+	 (result (condition-case err
+		     (eval (read-from-whole-string expr))
+		   (error (format "ERROR: %S" error)))))
+    (erc-send-message (format "%s ---> %S" expr result))))
+(defalias 'erc-cmd-E 'erc-cmd-EEVAL)
+(defun erc-cmd-SEVAL (&rest expression)
+  "evaluate given lisp expression."
+  (let* ((expr (mapconcat 'identity expression " ")) ;(mapconcat 'identity expression " "))
+	 (result (condition-case err
+		     (sawfish-eval-noread (read-from-whole-string expr))
+		   (error (format "ERROR: %S" error)))))
+    (erc-send-message (format "%s ---> %s" expr result))))
+
+
+;;;; More legacy stuff to move over
+;;; I'm at least renaming the functions now.
+(defmacro nisp-erc-with-current-buffer (server target &rest body)
+  "Execute forms in BODY in buffer for SERVER/TARGET.
+See `nix-erc-get-buffer' for details on how SERVER and TARGET are handled."
+  (declare (indent 1) (debug t))
+  (let ((buf (make-symbol "buf")))
+	`(let* ((,buf (nisp-erc-get-buffer ,target ,server)))
+		(if (eq ,buf nil) (nil)
+		  (with-current-buffer ,buf 
+		    ,@body)))))
+
+(defun nisp-erc-send-message (channel message &optional server)
+  "Send a MESSAGE to CHANNEL.
+If SERVER is given send message to CHANNEL on that SERVER,
+otherwise send message to the first CHANNEL on any server"
+  (nisp-erc-with-current-buffer server channel
+			       (erc-send-message message)))
+
+
+(defun nisp-erc-get-buffer (target &optional server-name)
+  "Get an ERC buffer on any server/channel.
+Both arguments are strings or nil.
+
+If TARGET only is passed, return first channel match or nil.
+
+If TARGET and SERVER-NAME is passed, return buffer on server that  matches
+or nil.
+
+IF TARGET is nil and SERVER-NAME is passed, return related server buffer"
+  (if (eq target nil)
+      (process-contact
+       (nisp-erc-get-server-proc server-name) :buffer)
+    (erc-get-buffer target (nisp-erc-get-server-proc server-name))))
+
+(defun nisp-erc-get-server-proc (&optional server-name)
+  "Return server process matching SERVER-NAME or return nil on failure"
+  (catch 'info
+    (mapcar (lambda (proc) 
+	      (let ((host (process-contact proc :host)))
+		(cond 
+		 ((string-equal server-name host)
+		  (throw 'info proc)))))
+	    (process-list))
+    nil))				;no match return nil.
+
+
 (provide 'nisp-old)
