e94dd564b421098f11c28d7755bb0902c0eef88f
james <james@localhost.(none)>
2010-01-07 00:55:14 +0000

    define nisp and nisp-paths defcustom groups

--- start.el
+++ start.el
@@ -2,6 +2,28 @@
 ;;;;;;; Copyright (c) 2010 Nixeagle
 ;;;;;;; reuse is granted under GPLv3 or later
 
+;;;; Customize stuff
+;;; We do the main nisp customization group and variables first. Little
+;;; to no chance of errors here.
+(defconst *nisp-version* "0.0.13"
+  "Nusp version.")
+(defvar *nisp-root-parent-group* 'emacs
+  "*Parent group of the group `nisp'.
+
+This is customizable for the only reason that I like to quickly
+reach my group from `custmize-browser' for testing defcustom
+elements. The normal user setting for this will likely be
+`local' (or some other sensible setting).")
+
+(defgroup nisp nil
+  "Nixeagle's lisp."
+  :version "0.0.13"                     
+  :group 'emacs)
+(defgroup nisp-paths nil
+  "Load path settings."
+  :version "0.0.13"
+  :group 'nisp)
+
 ;;;; "Bootstrap"
 ;;;We have to setup where we are in relation to the file system before
 ;;;much anything else can be done. This file is what is to be required
