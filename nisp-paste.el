b775734d1114547dc6cde86eb4fbb67ccad3af15
James S <i@nixeagle.org>
2010-01-22 16:47:55 +0000

    no trailing - if a message is not typed at the prompt. Release v0.6.4

--- nisp-paste.el
+++ nisp-paste.el
@@ -1,7 +1,7 @@
 ;;;;;; nisp-paste
 ;;;; Copyright (c) 2010 Nixeagle
 ;;;; Released under GNU GPLv3 or later
-;;; version: 0.6.3
+;;; version: 0.6.4
 ;;;
 ;;; Dependency on htmlize is included with the repo:
 ;;; (add-to-list 'load-path "/path/to/nisp.emacs") ; for this file
@@ -119,7 +119,7 @@ FUNCTIONS defaults to `nisp-paste-format-filename-functions'."
       (let ((url (concat nisp-paste-link-prefix paste-name)))
         (nisp-erc-send-message
          (buffer-name (car (erc-buffer-list nil)))
-         (concat "See " url " - " msg))))))
+         (concat "See " url (or msg " - " msg)))))))
 
 
 (provide 'nisp-paste)
