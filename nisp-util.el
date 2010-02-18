49f1ec72b74c5e4c9280365071d75c660dffbc9f
James S <i@nixeagle.org>
2010-02-18 09:28:12 +0000

    nisp-clean-whitespace

---
+++ nisp-util.el
@@ -0,0 +1,55 @@
+;;; nisp-util.el ---
+;;
+;; Filename: nisp-util.el
+;; Description:
+;; Author: James
+;; Maintainer:
+;; Created: Thu Feb 18 09:08:25 2010 (+0000)
+;; Version:
+;; Last-Updated:
+;;           By:
+;;     Update #: 3
+;; URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(defun nisp-clean-whitespace (n.string)
+  "Strip all newlines and multiple spaces from N.STRING."
+  (replace-regexp-in-string "^\s+\\|\s+$" ""
+                            (replace-regexp-in-string "[\s\n]+" " " n.string)))
+
+(provide 'nisp-util)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; nisp-util.el ends here
